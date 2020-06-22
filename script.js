c3ae6195f7ef271ff3895da77de2031d0b343348
Donkey <danglayers@gmail.com>
2020-06-22 01:24:56 -0400

    rewrote a few things

--- script.js
+++ script.js
@@ -37,12 +37,9 @@ function createCityList(citySearchList) {
       url: queryURL,
       method: "GET"
     })
-      // Store all of the retrieved data inside of an object called "weather"
       .then(function(weather) {
-        // Log the queryURL
         console.log(queryURL);
   
-        // Log the resulting object
         console.log(weather);
   
         var nowMoment = moment();
@@ -81,7 +78,6 @@ function createCityList(citySearchList) {
         $.ajax({
           url: queryURL3,
           method: "GET"
-          // Store all of the retrieved data inside of an object called "uvIndex"
         }).then(function(uvIndex) {
           console.log(uvIndex);
   
@@ -95,14 +91,11 @@ function createCityList(citySearchList) {
           $.ajax({
             url: queryURL2,
             method: "GET"
-            // Store all of the retrieved data inside of an object called "forecast"
           }).then(function(forecast) {
             console.log(queryURL2);
   
             console.log(forecast);
-            // Loop through the forecast list array and display a single forecast entry/time (5th entry of each day which is close to the highest temp/time of the day) from each of the 5 days
             for (var i = 6; i < forecast.list.length; i += 8) {
-              // 6, 14, 22, 30, 38
               var forecastDate = $("<h5>");
   
               var forecastPosition = (i + 2) / 8;
@@ -168,7 +161,7 @@ function createCityList(citySearchList) {
         .toLowerCase();
   
       if (city != "") {
-        //Check to see if there is any text entered
+        
       
         citySearchList[city] = true;
       localStorage.setItem("citySearchList", JSON.stringify(citySearchList));
